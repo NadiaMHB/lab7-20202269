6a217fb4f53904e616f53a3943eb8ccaddc337d4
NadiaMHB <nhb_02@hotmail.com>
2024-10-10 19:05:19 -0500

    first commit

---
+++ src/main/java/org/example/lab720202269/Lab720202269Application.java
@@ -0,0 +1,13 @@
+package org.example.lab720202269;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720202269Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab720202269Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lab720202269/Lab720202269ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab720202269;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720202269ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
